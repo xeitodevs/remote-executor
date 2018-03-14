92ca79e5fced7dbcf0f0401cc604a22a3d2ef1b5
Andrea Zucchini García <azga84@gmail.com>
2018-03-14 20:01:34 +0100

    create worker to execute commands

--- transport/transport.go
+++ transport/transport.go
@@ -1,5 +1,21 @@
 package transport
 
+import "github.com/xeitodevs/remote-executor/transport/command"
+
 type Transport interface {
-	ExecuteCommand(command string, server string, channel chan<- string)
+	Worker(commandsQueue command.Queue)
+}
+
+type transport struct {
+	commandsQueue <-chan *command.Command
+}
+
+func (_ *transport) Worker(commandsQueue command.Queue) {
+	for nextCommand := range commandsQueue.Queue() {
+		nextCommand.Exec()
+	}
+}
+
+func New() *transport {
+	return &transport{}
 }
