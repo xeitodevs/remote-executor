d6f4764b278fe6234bb9fedf48c18005246052ab
Andrea Zucchini García <azga84@gmail.com>
2018-03-14 20:01:58 +0100

    use command queue

--- main.go
+++ main.go
@@ -5,27 +5,41 @@ import (
 	"os"
 	"github.com/xeitodevs/remote-executor/files"
 	"github.com/xeitodevs/remote-executor/transport"
+	"github.com/xeitodevs/remote-executor/transport/command"
 	"fmt"
+	"time"
+	"github.com/xeitodevs/remote-executor/transport/ssh"
 )
 
-func main() {
+const numOfWorkers = 10
 
+func main() {
 	args := os.Args[1:]
 	configFile := args[0]
-	command := args[1]
+	userCommand := args[1]
 
 	hosts, err := files.FileLinesExtractor(configFile)
 	if err != nil {
 		log.Fatal("Cannot parse input hosts file.")
 	}
 
+	delta := len(hosts)
 	responses := make(chan string)
-	calls := 0
-	for _, host := range hosts {
-		go transport.ExecuteCommand(command, host, responses)
-		calls++
+	commandQueue := command.NewCommandQueue(delta)
+	for worker := 1; worker <= numOfWorkers; worker++ {
+		go transport.New().Worker(commandQueue)
 	}
-	for i := 0; i < calls; i++ {
+	for id, host := range hosts {
+		commandQueue.Add(&command.Command{
+			Value:           userCommand,
+			CreatedOn:       time.Now(),
+			Id:              id,
+			ChannelResponse: responses,
+			SSHAdapter:      ssh.New(host),
+		})
+	}
+	commandQueue.Close()
+	for i := 0; i < len(hosts); i++ {
 		fmt.Println(<-responses)
 	}
 }
